9a3255b321f15b99c2b98bccc11d4051a94272d6
Dave McConeghy <david.mcconeghy@gmail.com>
2021-11-16 11:15:08 -0500

    Adds reset option at game's end; Updates code comments

--- connect-four/README.md
+++ connect-four/README.md
@@ -1,6 +1,24 @@
-My version of Connect Four was coded for unit 11 of Springboard's Software Engineering Career Track. 
-It features a turn indicator on mouseover, a falling CSS animation, and proper handling of piece placement and win condition checking. 
+# Connect Four
 
-Future feature additions could include visual overhauls, futher mouseover refinements (to remove the prior color once clicked while the mouse has not yet moved), and better win/tie modals.
+My initial version of Connect Four was coded for unit 11 of *Springboard's Software Engineering Career Track* in November of 2021.
 
-The codebase is awaiting review prior to basic refactoring to reduce duplicate code.  
+### Current Features:
+
+- Turn/color indicator on mouseover with dynamic text further indicating which player's pieces are next
+- Placed pieces falling to their lowest avaialbe spot with CSS animation 
+- Proper win condition checking with alerts for wins by connecting 4 or ties from a full board.
+- Prevention of adding futher pieces if gameplay has concluded
+
+#### Known Bugs:
+
+One known bug is that the falling height of the pieces is a static 300. 
+This means the more pieces are in a column, the higher above the board the played piece drops from. 
+A potential but unimplemented fix is to have the pieces dynamically set their falling height, reducing each drop by 50px for each piece already in a column.
+
+#### Potential Additions & Features
+
+Future feature additions could include visual overhauls, better win/tie modals, and a reset button. 
+A reach feature would be to further animate all the pieces falling out the bottom of the board. 
+
+#### What's next for this project
+The codebase is awaiting review by my program mentors prior to further refactoring to make this project employ class defined objects.  
--- connect-four/connect4.js
+++ connect-four/connect4.js
@@ -7,21 +7,20 @@
 
 const WIDTH = 7;
 const HEIGHT = 6;
-let currPlayer = 1; //
-let board = []; // array of rows, each row is array of cells  (board[y][x])
-let tds;
+let currPlayer = 1; 
+let board = []; // global holder to mark player piece locations
+let tds; // a global holder for the column-top row for dynamic mouseover player/color swapping
 
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
+// makeBoard: creates an array of rows where each row is array of cells  (board[y][x])
 function makeBoard() {
   // board is an array whose rows number HEIGHT
   // for each row we create, map it with an array of WIDTH length
   // for each mapped item, fill it with value null
+
   board = [...Array(HEIGHT)].map(() => Array(WIDTH).fill(null));
 }
 
-/** makeHtmlBoard: make HTML table and row of column tops. */
+// makeHtmlBoard: make HTML table and row of column tops. 
 function makeHtmlBoard() {
   const htmlBoard = document.getElementById("board")
 
@@ -33,13 +32,11 @@ function makeHtmlBoard() {
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
   
-
- 
-      // for x set as the width of the game board
-      // create a table data cell held in variable headCell
-      // set the table data item's id to its x or column value
-      // append it to the table row created above
-      //append the headCell table row to the board
+    // for x set as the width of the game board
+    // create a table data cell held in variable headCell
+    // set the table data item's id to its x or column value
+    // append it to the table row created above
+    // append the headCell table row to the board
 
   for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
@@ -51,18 +48,12 @@ function makeHtmlBoard() {
 
   htmlBoard.append(top);
 
-  // tds = document.getElementById("column-top").children;
-
-  // for (let i = 0; i<tds.length; i++){
-  //   tds[i].addEventListener()
-  // }
-
-      //for each row of HEIGHT in the game board
-      // create a table row named row
-      // for the WIDTH of the game, create a table data item named cell
-      // set the cell's id to be its row-column or Y-X
-      // append the cell to each row for each WIDTH
-      // append the row to the board for each HEIGHT
+    //for each row of HEIGHT in the game board
+    // create a table row named row
+    // for the WIDTH of the game, create a table data item named cell
+    // set the cell's id to be its row-column or Y-X
+    // append the cell to each row for each WIDTH
+    // append the row to the board for each HEIGHT
 
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
@@ -77,8 +68,11 @@ function makeHtmlBoard() {
   }
 }
 
-/** findSpotForCol: given column x, return top empty y (null if filled) */
+// findSpotForCol: given column x, return top empty y (null if filled)
 function findSpotForCol(x) {
+    //starting from the lowest Y value decrement upward to highest Y
+    //checking for null locations where no pieces have been placed
+
   for (let lowestY = HEIGHT-1; lowestY >= 0; lowestY--){
       if(!board[lowestY][x]){
         return lowestY;
@@ -87,8 +81,10 @@ function findSpotForCol(x) {
   return null;
 }
 
-/** placeInTable: update DOM to place piece into HTML table of board */
+// placeInTable: update DOM to place piece into HTML table of board 
 function placeInTable(y, x) {
+  // create a game piece div and give it the classes piece and player1/2
+  // append the piece to the div that matches the inputed y, x 
   const gamePiece = document.createElement("div")
   gamePiece.classList.add("piece");
   gamePiece.classList.add(`player${currPlayer}`)
@@ -96,21 +92,21 @@ function placeInTable(y, x) {
 }
 
 /** endGame: announce game end */
-
 function endGame(msg) {
-  alert(msg);
+  let playAgain = confirm(`${msg} Want to play again?`);
+  if (playAgain){
+    location.reload();
+  } else {
+    return;
+  }
 }
 
 /** handleClick: handle click of column top to play piece */
-
 function handleClick(evt) {
 
-  //(currPlayer === 1) ? (document.getElementById("column-top").style.backgroundColor = "red") : (document.getElementById("column-top").style.backgroundColor = "blue")
-  
   // get x from ID of clicked cell
   let x = +evt.target.id;
   
-
   // get next spot in column (if none, ignore click)
   let y = findSpotForCol(x);
   if (y === null) {
@@ -125,8 +121,13 @@ function handleClick(evt) {
 
   // check for win
   if (checkForWin()) {
+    //grab the current Player to set the color
     let color = currPlayer === 1 ? "blue" : "red"
 
+    //update the HUD
+    // wait 1100ms so that the piece appears on the board before the win alert displays
+    //remove the event listeners so that no more pieces can be placed
+    //leave the function so that the HUD is not updated again
     document.getElementById("currentPlayer").innerText = `Player ${currPlayer} / ${color} won!`;
     setTimeout(() => {return endGame(`Player ${currPlayer} / ${color} won!`)}, 1100);
     document.getElementById("column-top").removeEventListener("click", handleClick);
@@ -142,19 +143,15 @@ function handleClick(evt) {
     return;
   }
 
-  // switch players
+  // If no winner or no tie, switch players
   currPlayer = currPlayer === 1 ? 2 : 1;
   
+  //grab the current player color and update the HUD
   color = currPlayer === 1 ? "blue" : "red"
   document.getElementById("currentPlayer").innerText = `It's now ${color}'s turn`;
-
-  // this should turn the background empty on click but it doesn't work bc mouseover is still active. 
-  // evt.currentTarget.style.backgroundColor = ""
- 
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
   function _win(cells) {
     // Check four cells to see if they're all color of current player
@@ -171,10 +168,10 @@ function checkForWin() {
     );
   }
 
-//for each row
-//for each column in the row
+//for each row & then for each column in the row
 //check horizontal victory by looking at the four cells and seeing if they are all the same player
-//check vertical, diagonal L and diagonal R for same. The changes to the X, Y indicate the cells to check starting from initial y, x
+//check vertical, diagonal down left and diagonal down right for same. 
+// The changes to the X, Y indicate the cells to check starting from initial y, x
 // if any one of the four directions returns T from _win then the function checkForWin returns T
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
@@ -194,16 +191,16 @@ makeBoard();
 makeHtmlBoard();
 
 
-//These two functions manage the mouseover color swap to indicate turn and location
-//One bug is that once clicked the color won't change until the mouse has moved out of its currentTarget.
-
+//handleHover & handleExitHover manage the mouseover color swap to indicate turn and location
 function handleHover(e){
+    //if no one has won, change the background color of the column-top background piece to the current player
+    //if a player has won or the players have tied, then remove all background colors from column-top
+  if (!checkForWin()){
+      (currPlayer === 1) ? (e.currentTarget.style.backgroundColor = "blue") : (e.currentTarget.style.backgroundColor = "red")
+  } else e.currentTarget.style.backgroundColor = ""
+  }
 
-if (!checkForWin()){
-    (currPlayer === 1) ? (e.currentTarget.style.backgroundColor = "blue") : (e.currentTarget.style.backgroundColor = "red")
-} else e.currentTarget.style.backgroundColor = ""
-}
-
+//We must check each column / td in column-top for mouseout to remove the color if the cursor moves to a different column
 function handleExitHover(e){
   if (!checkForWin()){
     e.currentTarget.style.backgroundColor = ""
