32918fdc653a30788a0406cc1a20da284d2fdf24
Dave McConeghy <david.mcconeghy@gmail.com>
2021-12-10 12:34:58 -0500

    Adds initial templates

--- flask/flask-boggle/app.py
+++ flask/flask-boggle/app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,3 +9,9 @@ app.config['SECRET_KEY'] = "daves-boggle"
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route("/")
+def start_page():
+    """ User begins boggle game"""
+
+    return render_template("start.html")
