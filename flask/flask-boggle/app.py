a6a1d862aefa629147af08a8036ee7335521b55e
Dave McConeghy <david.mcconeghy@gmail.com>
2021-12-10 14:28:51 -0500

    Begins work on JS interface for game logic

--- flask/flask-boggle/app.py
+++ flask/flask-boggle/app.py
@@ -1,5 +1,6 @@
+from flask.json import jsonify
 from boggle import Boggle
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -11,7 +12,6 @@ debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 BOARD = "board"
-ANSWERS = "answers"
 
 @app.route("/")
 def home_page():
@@ -19,38 +19,25 @@ def home_page():
 
     return render_template("start.html")
 
-@app.route("/boggle", methods=["POST"])
+@app.route("/boggle")
 def boggle_start():
     """ Boggle board appears"""
 
     board = boggle_game.make_board()
     session[BOARD] = board
+    
 
     return render_template("boggle.html", board=board)
 
-@app.route("/submission", methods=["POST"])
-def submission_handler():
-    """This collects the answer & checks its validity
-        If valid, it then appends the words found list.
-        Then it scrubs the text form and awaits a new submission
+@app.route("/validate")
+def validate_submission():
+    """ Check the dictionary for the submitted word.
+        Returns JSON for JS use.
     """
-    #get the answers list
-    answers = session[ANSWERS]
 
-    #get the answer
-    answer = request.form['submit_word']
-    
-    #check if valid word and append or reject
-    if boggle_game.check_valid_word(answer):
-        answers.append(answer)
-        flash(f"Great! We added that word to your list.")
-    else :
-        flash(f"Sorry, that isn't a valid word.")
-    
-    # Get the board from the session to re-pass on load
+    submission = request.args["submit_word"]
     board = session[BOARD]
+    check = boggle_game.check_valid_word(board, submission)
 
+    return jsonify({'result': check})
 
-    return render_template("boggle.html", 
-                            board=board, 
-                            )
