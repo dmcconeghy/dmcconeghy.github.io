bde39d573d2d04275d67d52eaa2607424dd4136e
Dave McConeghy <david.mcconeghy@gmail.com>
2022-01-16 14:57:01 -0500

    Initial part one skeleton SQLAlchemy with CRUD routes

--- flask/flask-blogly/models.py
+++ flask/flask-blogly/models.py
@@ -1,1 +1,29 @@
-"""Models for Blogly."""
+"""SQLAlchemy models for blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "/static/blank_profile.png"
+
+class User(db.Model):
+    """Site user."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    @property
+    def full_name(self):
+        """Return full name of user."""
+
+        return f"{self.first_name} {self.last_name}"
+
+
+def connect_db(app):
+
+    db.app = app
+    db.init_app(app)
