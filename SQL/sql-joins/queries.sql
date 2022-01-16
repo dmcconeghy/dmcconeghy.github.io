ad7e2f6a7b313d3ebdf11630944bcefcf1134f74
Dave McConeghy <david.mcconeghy@gmail.com>
2022-01-16 16:58:43 -0500

    Updates joins and queries SQL exercises

--- SQL/sql-joins/queries.sql
+++ SQL/sql-joins/queries.sql
@@ -1,1 +1,10 @@
 -- write your queries here
+SELECT * FROM owners FULL OUTER JOIN vehicles ON owners.id = vehicles.owner_id;
+
+SELECT first_name, last_name, COUNT(*) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id GROUP BY first_name, last_name ORDER BY first_name ASC;
+
+SELECT first_name, last_name, ROUND(AVG(price)) as average, COUNT(owner_id) 
+  FROM owners JOIN vehicles ON owners.id = vehicles.owner_id 
+  GROUP BY first_name, last_name 
+  HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000
+  ORDER BY first_name DESC; 
