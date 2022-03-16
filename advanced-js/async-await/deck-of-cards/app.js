af99dec298f306e498e5c7acd9661e510d2ec489
Dave McConeghy <david.mcconeghy@gmail.com>
2022-03-16 14:43:21 -0400

    Adds async-await version of deck-of-cards and number-facts

--- advanced-js/async-await/deck-of-cards/app.js
+++ advanced-js/async-await/deck-of-cards/app.js
@@ -2,58 +2,61 @@ $(function () {
     let BASEURL = "http://deckofcardsapi.com/api/deck"
 
     // Console log a single card from a new deck 
-    $.getJSON(`${BASEURL}/new/draw`).then(data => {
+    async function first() { 
+        let data = await $.getJSON(`${BASEURL}/new/draw`)
         let { suit, value } = data.cards[0];
         console.log("Single Card: ", suit, value)
-    })
-
+    }
+    
+    first()
 
 
     // Console.log a single card from a deck
-    let firstCard = null;
-    $.getJSON(`${BASEURL}/new/draw`).then(data => 
-        {
-        firstCard = data.cards[0];
-        let deck_id = data.deck_id;
-        return $.getJSON(`${BASEURL}/${deck_id}/draw`)
-    }).then(data => {
-        let secondCard = data.cards[0];
-        [firstCard, secondCard].forEach(function(card) {
-            console.log("Same deck single card: ", card.value, card.suit)
-        })
-    })
+    async function second() {
+        let firstCardData = await $.getJSON(`${BASEURL}/new/draw`);
+        let deck = firstCardData.deck_id;
+        let secondCardData = await $.getJSON(`${BASEURL}/${deck}/draw`);
+        [firstCardData, secondCardData].forEach(card => {
+          let { suit, value } = card.cards[0];
+          console.log(value, "of", suit);
+        });
+      }
+
+    second()
+   
 
     // Draw through a shuffled deck
+    async function third() {
+        let $btn = $('button');
+        let $table = $('#table')
 
-    let deck = null;
-    let $btn = $('button');
-    let $table = $('#table')
+        let deckData = await $.getJSON(`${BASEURL}/new/shuffle`)
 
-    $.getJSON(`${BASEURL}/new/shuffle`).then(data => {
-        deck = data.deck_id
-        $btn.show()
-    });
+        $btn.show().on('click', async function() {
+            $.getJSON(`${BASEURL}/${deckData.deck_id}/draw`)
+                let cardData = await $.getJSON(`${BASEURL}/${deckData.deck_id}/draw`)
+                let cardSrc = cardData.cards[0].image
+                let angle = Math.random() * 90 - 45;
+                let randomX = Math.random() * 40 - 20;
+                let randomY = Math.random() * 40 - 20;
+                $table.append(
+                    $('<img>', {
+                        src: cardSrc,
+                        css: {
+                            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+                        }
+                    })
+                );
+                if (cardData.remaining === 0) { 
+                    $btn.remove() 
+                    alert("No more cards")
+                }
+            })
+        }    
+    
 
-    $btn.on('click', function(){
-        $.getJSON(`${BASEURL}/${deck}/draw`).then(data => {
-            let cardSrc = data.cards[0].image
-            let angle = Math.random() * 90 - 45;
-            let randomX = Math.random() * 40 - 20;
-            let randomY = Math.random() * 40 - 20;
-            $table.append(
-                $('<img>', {
-                    src: cardSrc,
-                    css: {
-                        transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
-                    }
-                })
-            );
-            if (data.remaining === 0) { 
-                $btn.remove() 
-                alert("No more cards")
-            }
-        })
-    })
+    third()
+    
 })
 
 
--- advanced-js/async-await/number-facts/app.js
+++ advanced-js/async-await/number-facts/app.js
@@ -3,9 +3,14 @@ let BASEURL = "http://numbersapi.com"
 let randomGen = () => { return Math.floor(Math.random()*100) }
 
 // This is the basic route and prints a sample to console.
-$.getJSON(`${BASEURL}/${randomGen()}/?json`).then(data => {
-    console.log(data.text);
-});
+async function randomNumberFact () {
+    return await $.getJSON(`${BASEURL}/${randomGen()}/?json`).then(data => {
+        console.log(data.text);
+    });
+}
+
+randomNumberFact()
+
 
 let randomNumbers = () => {
     let temp = []
@@ -16,17 +21,28 @@ let randomNumbers = () => {
 }
 
 // This request gets four random facts and returns them as a JSON object.
-$.getJSON(`${BASEURL}/${randomNumbers()}?json`).then(data => {
+async function fourRandomNumberFacts () {
+ return await $.getJSON(`${BASEURL}/${randomNumbers()}?json`).then(data => {
     console.log(data);
   });
+}
+
+fourRandomNumberFacts()
 
 
 // This request calls four times with a different random number each time. 
 // It then appends all results to the body of index.html
-Promise.all(
-    Array.from({ length: 4}, () => {
-        return $.getJSON(`${BASEURL}/${randomGen()}/?json`);
+
+async function printFourFacts() {
+    let fourFacts = await Promise.all(
+        Array.from({ length: 4}, () => {
+            return $.getJSON(`${BASEURL}/${randomGen()}/?json`);
+        })
+    );
+
+    fourFacts.forEach(data => {
+        $("body").append(`<div><h3>A Random Number Fact:</h3><p>${data.text}</p><div>`)
     })
-).then(facts => {
-    facts.forEach(data => $("body").append(`<div><h3>A Random Number Fact:</h3><p>${data.text}</p><div>`));
-})
+}  
+    
+printFourFacts()
