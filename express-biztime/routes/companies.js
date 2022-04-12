a2697ebbbc923ec05dfb9722c9b3eaa35bdbb5ea
Dave McConeghy <david.mcconeghy@gmail.com>
2022-04-12 11:53:50 -0400

    Initial commit of biztime pre further study

---
+++ express-biztime/routes/companies.js
@@ -0,0 +1,132 @@
+const express = require("express");
+const db = require("../db");
+const ExpressError = require("../expressError");
+const slugify = require("slugify");
+
+let router  = new express.Router();
+
+router.get("/", async function(req, res, next) {
+
+    try {
+
+        const results = await db.query(
+            `SELECT code, name 
+             FROM companies
+             ORDER BY name`);
+
+        return res.json({"companies": results.rows});
+    
+    } catch (err) {
+
+        return next(err)
+    
+    }
+         
+});
+
+router.get("/:code", async function(req, res, next) {
+    try {
+        const code = req.params.body
+
+        const companyResult = await db.query(
+            `SELECT code, name, description
+             FROM companies 
+             WHERE code=$1`, 
+             [code]
+             );
+        
+        const invoiceResult = await db.query(
+            `SELECT id
+             FROM invoices
+             WHERE comp_code=$1`,
+             [code]
+            );   
+        
+        if (companyResult.rows.length === 0){
+            throw new ExpressError(`Company code ${code} not found`, 404);
+        }
+        const company = companyResult.rows[0];
+        const invoices = invoiceResult.rows;
+
+        company.invoices = invoices.map(inv => inv.id);
+
+        return res.json({"company": company});
+
+    } catch (err) {
+
+        return next(err)
+    }
+})
+
+router.post("/", async function(req, res, next) {
+    try {
+
+        const { name, description } = req.body;
+        const code = slugify(name, {lower: true});
+
+        const result = await db.query(
+            `INSERT INTO companies (code, name, description)
+             VALUES($1, $2, $3)
+             RETURNING code, name, description`,
+             [code, name, description]
+        );
+
+        return res.status(201).json({"company": result.rows[0]});
+
+    } catch(err) {
+
+        return next(err)
+    }
+})
+
+router.put("/:code", async function(req, res, next) {
+    try {
+
+        const { name, description } = req.body;
+        const code = req.params.code;
+
+        const result = await db.query(
+            `UPDATE companies 
+             SET name=$1, description=$2 
+             WHERE code = $3
+             RETURNING code, name, description`,
+             [name, description, code]
+        );
+
+        if (result.rows.length === 0){
+            throw new ExpressError(`Company code ${code} doesn't exist`, 404);
+        } else {
+            return res.json({"company": result.rows[0]});
+        }
+
+    } catch (err) {
+
+        return next(err)
+    }
+})
+
+
+// NEEDS 404
+router.delete("/:code", async function(req, res, next) {
+    try {
+
+        const result = await db.query(
+            `DELETE FROM companies 
+             WHERE code = $1`, 
+             [req.params.code]
+            );
+        
+        if (result.rows.length === 0){
+            throw new ExpressError(`Company code ${code} doesn't exist`, 404);
+        } else {
+            return res.json({"status": "deleted"});
+        }
+
+    } catch(err) {
+
+        return next(err)
+    }
+})
+
+
+module.exports = router;
