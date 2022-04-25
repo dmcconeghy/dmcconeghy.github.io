44795dff42697ee45a157f9d9e67236c747367a7
Dave McConeghy <david.mcconeghy@gmail.com>
2022-04-25 12:07:07 -0400

    Updates App with job routing; Updates job/jobs models and routes. Adds jobNew and jobUpdate validator schema.

--- express-jobly/models/job.js
+++ express-jobly/models/job.js
@@ -5,7 +5,134 @@ const { BadRequestError, NotFoundError } = require("../expressError");
 const { sqlForPartialUpdate } = require("../helpers/sql");
 
 class Job {
+  /** Create a job (from data), update db, return new job data.
+   *
+   * data should be { title, salary, equity, company_handle }
+   *
+   * Returns { id, title, salary, equity, company_handle }
+   *
+   * Throws BadRequestError if job already in database.
+   * */
 
+  static async create({ title, salary, equity, company_handle }){
+    const duplicateCheck = await db.query(
+        `SELECT title
+         FROM jobs
+         WHERE title = $1 AND 
+         company_handle = $2`,
+         [title, company_handle]);
+    
+    if (duplicateCheck.rows[0]){
+        throw new BadRequestError(`Duplicate job at ${company_handle} named ${title}`);
+    }
+
+    const result = await db.query(
+        `INSERT INTO jobs
+        (title, salary, equity, company_handle)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, title, salary, equity, company_handle AS "companyHandle"`,
+        [ title, salary, equity, company_handle]
+    )
+    
+    const job = result.rows[0];
+
+    return job;
+  }
+
+   /** Find all jobs.
+   *
+   * Returns [{ title, salary, equity, company_handle }, ...]
+   * */
+
+   static async findAll() {
+       const jobsRes = await db.query(
+           `SELECT title, salary, equity, company_handle AS "companyHandle"
+           FROM jobs
+           ORDER BY company_handle, title`);
+        
+        return jobsRes.rows
+   }
+
+   /** Find all companies using optional filters.
+   *   data should be { minEmployees = number > 0 < maxEmployees
+   *                    maxEmployees = number > 0 > minEmployees
+   *                    nameLike = alphanumeric of match(/^[a-z0-9_-]{3,15}$/)
+   *                  }   
+   *
+   * Returns [{ companies }] filtered by any combination of min/max/name.
+   * 
+   * Failures of invalid input throw custom BadRequestError
+   * 
+   * ADD THIS
+   * 
+   * */
+
+
+
+
+   /** Given a job id, return data about job.
+   *
+   * Returns { id, title, salary, equity, company_handle }
+   *
+   * Throws NotFoundError if not found.
+   **/
+
+    static async get(id){
+        const jobRes = await db.query(
+            `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+            FROM jobs
+            WHERE id = $1`,
+            [id]);
+
+        const job = jobRes.rows[0];
+
+        if (!job) throw new NotFoundError(`No matching jobs found`)
+
+        return job
+    }
+
+    static async update(id, data){
+        const { setCols, values } = sqlForPartialUpdate(
+            data, {
+                title: "title",
+                salary: "salary",
+                equity: "equity",
+            });
+        
+        const handleVarIdx = "$" + (values.length + 1);
+
+        const querySql =    `UPDATE jobs
+                             SET ${setCols}
+                             WHERE id = ${handleVarIdx}
+                             RETURNING  id, 
+                                        title,
+                                        salary,
+                                        equity,
+                                        company_handle AS "companyHandle"`;
+        const result = await db.query(querySql, [...values, id]);
+        const job = result.rows[0]
+
+        if (!company) throw new NotFoundError('No matching job to edit')
+
+        return job
+    }
+
+    /** Delete given company from database; returns undefined.
+    *
+    * Throws NotFoundError if company not found.
+    **/
+
+    static async remove(id) {
+        const result = await db.query(
+            `DELETE
+             FROM jobs
+             WHERE id = $1
+             RETURNING title, company_handle AS "companyHandle"`,
+             [id]);
+        const job = result.rows[0];
+
+        if (!job) throw new NotFoundError('Job not found.')
+    }
 }
 
 module.exports = Job
--- express-jobly/routes/jobs.js
+++ express-jobly/routes/jobs.js
@@ -13,3 +13,118 @@ const Job = require("../models/job");
 // const jobUpdateSchema = require("../schemas/jobUpdate.json");
 
 const router = new express.Router();
+
+/** POST / { job } =>  { job }
+ *
+ * job should be { title, salary, equity, company_handle }
+ *
+ * Returns { id, title, name, description, numEmployees, logoUrl }
+ *
+ * Authorization required: login & admin
+ */
+
+router.post("/", ensureAdmin, async function(req, res, next){
+    try {
+        const validator = jsonschema.validate(req.body, jobNewSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const job = await Job.create(req.body);
+        return res.status(201).json({ job });
+    } catch (err){
+        return next(err);
+    }
+});
+
+/** GET /  =>
+ *   { jobs: [ { id, title, salary, equity, company_handle }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - minSalary
+ * - hasEquity (boolean if T filter, if F list all)
+ * - titleLike (will find case-insensitive, partial matches)
+ *
+ * Authorization required: none
+ */
+
+router.get("/", async function(req, res, next) {
+    try {
+        const minSalary = req.query.minSalary || null;
+        const hasEquity = req.query.hasEquity || false;
+        const titleLike = req.query.titleLike || null;
+
+        //return all if no filters selected
+        if (!minSalary && !hasEquity && !titleLike){
+            const jobs = await Job.findAll();
+            // console.log("I ran no parameters")
+            return res.json({ jobs })
+        } else {
+            const jobs = await Job.findWithFilters(minSalary, hasEquity, titleLike)
+            //console.log("I ran some parameters")
+            return res.json({ jobs })
+        }
+    } catch (err){
+        return next(err);
+    }
+});
+
+/** GET /[id]  =>  { job }
+ *
+ *  Job is { id, title, salary, equity, company_handle }
+ *
+ * Authorization required: none
+ */
+
+router.get("/:id", async function(req, res, next){
+    try {
+        const job = await Job.get(req.params.id)
+
+        return res.json({job})
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** PATCH /[id] { fld1, fld2, ... } => { job }
+ *
+ * Patches job data.
+ *
+ * fields can be: { title, salary, equity }
+ *
+ * Returns { id, title, salary, equity, company_handle }
+ *
+ * Authorization required: login & admin
+ */
+
+router.patch("/:id", ensureAdmin, async function(req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, jobUpdateSchema);
+        if (!validator.valid){
+            const errs = validtor.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const job = await Job.update(req.params.id, req.body);
+        return res.json({ job });
+    } catch (err){
+        return next(err);
+    }
+});
+
+/** DELETE /[id]  =>  { deleted: id }
+ *
+ * Authorization: login & admin
+ */
+
+router.delete("/:id", ensureAdmin, async function(req, res, next){
+    try {
+        await Job.remove(req.params.id);
+        return res.json({deleted: req.params.id});
+    } catch (err){
+        return next(err);
+    }
+});
+
+module.exports = router;
